751db228a935d78e01072ce9694d49fdeca55b23
Ishwar Vishwakarma <ishwar.ckpcet@gmail.com>
2026-02-26 15:59:10 +0530

    Task 1 - Done!

---
+++ temperature_analysis.py
@@ -0,0 +1,33 @@
+# Name: Ishwar Vishwakarma
+# Roll Number: iitp_aimlh_2602411
+# Assignment: Python Loops & Automation - Subjective Question
+
+def MaxMinTemp():
+    print("\n===== Task 1: Find Maximum and Minimum =====\n")
+    
+    temperatures = [28, 32, 35, 29, 31, 27, 30]
+    
+    min_temp = temperatures[0]
+    max_temp = temperatures[0]
+
+    for temp in temperatures:
+        if(temp < min_temp):
+            min_temp = temp
+        if(temp > max_temp):
+            max_temp = temp
+    
+    print(f"Maximum Temperature : {max_temp}°C")
+    print(f"Minimum Temperature : {min_temp}°C")
+
+MaxMinTemp()
+
+"""
+print("\n===== Task 2: Count Hot Days =====")
+temperatures = [28, 32, 35, 29, 31, 27, 30]
+# Write your code here
+
+
+print("\n===== Task 3: Alert System =====")
+temperatures = [28, 32, 35, 40, 31, 33, 30]
+# Write your code here
+"""
