55acf70d2d745fca46c2cf4fe2f32ea7a048054a
Ishwar Vishwakarma <ishwar.ckpcet@gmail.com>
2026-02-26 16:06:28 +0530

    Task 2 - Also Done!

--- temperature_analysis.py
+++ temperature_analysis.py
@@ -19,14 +19,26 @@ def MaxMinTemp():
     print(f"Maximum Temperature : {max_temp}°C")
     print(f"Minimum Temperature : {min_temp}°C")
 
-MaxMinTemp()
+#MaxMinTemp()
+
+def CountHotDays():
+    print("\n===== Task 2: Count Hot Days =====\n")
+    
+    temperatures = [28, 32, 35, 29, 31, 27, 30]
+    
+    hot_days = 0
+    
+    for temp in temperatures:
+        if(temp <= 30):
+            continue
+        if(temp > 30):
+            hot_days += 1
+    
+    print(f"Hot Days (>30°C) : {hot_days}")
+    
+CountHotDays()
 
 """
-print("\n===== Task 2: Count Hot Days =====")
-temperatures = [28, 32, 35, 29, 31, 27, 30]
-# Write your code here
-
-
 print("\n===== Task 3: Alert System =====")
 temperatures = [28, 32, 35, 40, 31, 33, 30]
 # Write your code here
